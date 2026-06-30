6b01870cebdb003ec1fca69e72273f5083a6213f
sumit <sumitvsonawane2611@gmail.com>
2026-06-30 22:49:26 +0530

    Implemented logout feature

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { MatToolbar } from '@angular/material/toolbar';
-import { MatIcon } from '@angular/material/icon';
+import { MatToolbar, MatToolbarModule } from '@angular/material/toolbar';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { AuthService } from '../../../core/services/auth/auth.service';
+import { NotificationService } from '../../../core/services/notification/notification.service';
+import { Router } from '@angular/router';
+import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbar, MatIcon],
+  imports: [MatToolbarModule, MatIconModule,MatMenuModule,MatMenuTrigger],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  constructor(
+    private authService: AuthService,
+    private notificationService: NotificationService,
+    private router: Router,
+  ) {}
 
+  logout() {
+    this.authService.logout();
+    this.notificationService.success('Successfully logged out. See you soon!');
+    this.router.navigate(['/login']);
+  }
+
+  navProfile() {
+    // this.router.navigate(['/profile']);
+    console.log('Comming Soon');
+    
+  }
 }
