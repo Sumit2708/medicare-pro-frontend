df259a1752c9fca8964db145183235edc0932827
sumit <sumitvsonawane2611@gmail.com>
2026-09-21 19:39:18 +0530

    Add api path

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
 
   //   API_URL: 'http://localhost:3000',
-  API_URL: 'https://clinic-management-api-xzsc.onrender.com/',
+  API_URL: 'https://clinic-management-api-xzsc.onrender.com',
 };
